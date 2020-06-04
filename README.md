ac011c482000fc6ee5860d25ba9fa357343cba24
Masayuki Sunahara <tamanishi@gmail.com>
2020-06-04 19:52:29 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# イルカの状態をアレします
+
+apiが生えていることが分かったのでapi経由で
+
+apiの存在を知らなかった期間は[puppeteer経由](https://github.com/tamanishi/irucaState.git "puppeteer経由")で操作していました
+
---
+++ index.js
@@ -0,0 +1,27 @@
+const axios = require('axios')
+require('dotenv').config()
+
+let ROOM_CODE = process.env.ROOM_CODE
+let MEMBER_ID = process.env.MEMBER_ID
+
+const states = ["在席", "退社", "取込中", "離席", "外出", "休暇", "会議中", "帰社", "社外研修", "社内研修", "在宅勤務"]
+
+const url = `https://iruca.co/api/rooms/${ROOM_CODE}/members/${MEMBER_ID}`
+
+if (process.argv.length < 3) {
+    console.log('needs target status.')
+    process.exit(-1)
+}
+
+if (!states.includes(process.argv[2])) {
+    console.log('unsupported status : ' + process.argv[2])
+    process.exit(-1)
+}
+
+axios.put(url, {
+    status: process.argv[2]
+})
+.catch(function (error) {
+    console.log(error)
+})
+
